73abf95ee1de49c07105a19f609f14e7e2211143
Red Rapious <red.rapious@gmail.com>
2024-02-20 22:13:15 +0100

    Added a test framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -15,7 +18,25 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    panic!("Hello World!");
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial_assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
