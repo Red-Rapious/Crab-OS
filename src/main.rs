b96457da310323ecb1462aac9e2971aed60b8073
Red Rapious <red.rapious@gmail.com>
2024-02-21 15:13:07 +0100

    Tests now exit Qemu

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,30 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Succes = 0x10,
+    Failed = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Succes);
 }
 
 #[test_case]
